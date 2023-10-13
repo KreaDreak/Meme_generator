11d6e31ad7c8bb43925b2ae4d111e7e15c23673b
KreaDreak <anvarllatypov@gmail.com>
2023-10-13 20:47:45 +0500

    Memo generator v razrobotke, ojidayte

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+print('Мемо-генератор запущен, готовь мемы))')
+
+
+top_text = input('Введи верхний текст: ')
+bottom_text = input('Введи нижний текст: ')
+print('Генерация мема', end='')
+time.sleep(1)
+print('.', end='')
+
+time.sleep(1)
+print('.', end='')
+
+time.sleep(1)
+print('.')
+
+print(top_text)
+print(bottom_text)
