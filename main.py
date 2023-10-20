996cee468e06ed039f4fc2e3800fed2510a9f577
KreaDreak <anvarllatypov@gmail.com>
2023-10-20 20:18:59 +0500

    add choice of text type

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import time
 print('Мемо-генератор запущен, готовь мемы))')
 
 
-top_text = input('Введи верхний текст: ')
-bottom_text = input('Введи нижний текст: ')
+top_text = ''
+bottom_text = ''
+
+
+text_type = int(input('Введите 1, если нужен только нижний текст, и 2, если и верхний, и нижний: '))
+
+if text_type == 1:
+    bottom_text = input('Введи нижний текст: ')
+else:
+    top_text = input('Введи верхний текст: ')
+    bottom_text = input('Введи нижний текст: ')
+
 print('Генерация мема', end='')
 time.sleep(1)
 print('.', end='')
